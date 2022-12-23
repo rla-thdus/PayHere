842988d53b27f0fc92d9d565ca173ff1b4af27a0
rla_thdus <0228ksy@naver.com>
2022-12-23 20:05:08 +0900

    Test: 로그인 상태인 유저의 로그아웃 요청 성공하는 테스트 추가

---
+++ accounts/tests/test_logout.py
@@ -0,0 +1,20 @@
+import json
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class LogoutTest(APITestCase):
+    def setUp(self):
+        data = {
+            "email": "test@test.com",
+            "password": "test1234",
+        }
+        self.client.post('/accounts/registration', data)
+        self.login = self.client.post('/accounts/login', data)
+
+    def test_logout_should_success_with_authenticated_user(self):
+        headers = {'HTTP_AUTHORIZATION': "Bearer " + json.loads(self.login.content)['access_token']}
+        response = self.client.delete('/accounts/logout', None, **headers)
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        self.assertEqual(response.data['message'], 'Logout success')
