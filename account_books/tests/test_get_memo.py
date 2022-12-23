5e322ec911e07df39b085258dca90910bcbe9cde
rla_thdus <0228ksy@naver.com>
2022-12-23 22:20:55 +0900

    Test: 자신의 메모가 아닐 경우 권한 문제로 실패하는 테스트 추가

--- account_books/tests/test_get_memo.py
+++ account_books/tests/test_get_memo.py
@@ -10,8 +10,14 @@ class MemoAddAPITest(APITestCase):
             "email": "test@test.com",
             "password": "test1234",
         }
+        new_user_data = {
+            "email": "test1@test.com",
+            "password": "test1234",
+        }
         self.client.post('/accounts/registration', user_data)
+        self.client.post('/accounts/registration', new_user_data)
         self.login = self.client.post('/accounts/login', user_data)
+        self.new_login = self.client.post('/accounts/login', new_user_data)
         self.data = {
             "spend_price": 10000,
             "content": 'buy snacks'
@@ -36,4 +42,9 @@ class MemoAddAPITest(APITestCase):
         response = self.client.get(f'/account_books/memos/{self.memo_id + 1}', None, **headers)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertEqual(response.data['message'], 'invalid memo id')
+
+    def test_get_memo_should_fail_not_own_memo(self):
+        headers = {'HTTP_AUTHORIZATION': "Bearer " + json.loads(self.new_login.content)['access_token']}
+        response = self.client.get(f'/account_books/memos/{self.memo_id}', None, **headers)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
