0405797c1c1765708365db14968310dd2d38aee8
rla_thdus <0228ksy@naver.com>
2022-12-23 19:25:13 +0900

    Test: 등록된 계정과 일치하지 않는 경우 로그인 실패하는 테스트 추가

--- accounts/tests/test_login.py
+++ accounts/tests/test_login.py
@@ -14,3 +14,8 @@ class LoginTest(APITestCase):
         response = self.client.post('/accounts/login', self.data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertTrue('access_token' in response.data)
+
+    def test_login_should_fail_with_wrong_account_info(self):
+        self.data['password'] = '1234'
+        response = self.client.post('/accounts/login', self.data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
