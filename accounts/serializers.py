40cbeca83a8c2d1022d266a89df7349753cb2d71
rla_thdus <0228ksy@naver.com>
2022-12-23 18:59:26 +0900

    Feat: 회원가입 성공시에 응답 데이터에 id, email, created_at만 나오게 수정

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,7 +6,7 @@ class UserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'email', 'password', 'created_at']
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
