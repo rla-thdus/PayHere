cfd4afa0a066de166c085ec60d791fc10e239538
rla_thdus <0228ksy@naver.com>
2022-12-23 22:13:02 +0900

    Test: 존재하지 않는 memo id를 요청한 경우 실패하는 테스트 추가

--- account_books/tests/test_get_memo.py
+++ account_books/tests/test_get_memo.py
@@ -30,3 +30,10 @@ class MemoAddAPITest(APITestCase):
     def test_get_memo_should_fail_with_not_authenticated_user(self):
         response = self.client.get(f'/account_books/memos/{self.memo_id}')
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_get_memo_should_fail_with_not_exists_memo(self):
+        headers = {'HTTP_AUTHORIZATION': "Bearer " + json.loads(self.login.content)['access_token']}
+        response = self.client.get(f'/account_books/memos/{self.memo_id + 1}', None, **headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data['message'], 'invalid memo id')
+
