d096a45f88e18af1be1ad7a2b19dd050bde82e59
rla_thdus <0228ksy@naver.com>
2022-12-23 20:11:48 +0900

    Feat: 가계부 구현할 account books 앱 생성

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
-    'accounts'
+    'accounts',
+    'account_books'
 ]
 
 REST_FRAMEWORK = {
