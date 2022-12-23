42675c6743ac466bc0c7e83118951139787ee6fd
rla_thdus <0228ksy@naver.com>
2022-12-23 21:57:13 +0900

    Feat: 메모 상세 데이터 확인하는 기능 구현

--- account_books/urls.py
+++ account_books/urls.py
@@ -6,4 +6,5 @@ app_name = "account_books"
 
 urlpatterns = [
     path('memos', views.MemoAPI.as_view()),
+    path('memos/<int:memo_id>', views.MemoDetailAPI.as_view()),
 ]
--- account_books/views.py
+++ account_books/views.py
@@ -3,14 +3,36 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from account_books.models import Memo
 from account_books.serializers import MemoSerializer
+from config.permissions import IsMine
 
 
 class MemoAPI(APIView):
     permission_classes = [IsAuthenticated]
+
     def post(self, request):
         serializer = MemoSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MemoDetailAPI(APIView):
+    permission_classes = [IsAuthenticated, IsMine]
+
+    def get_object(self, memo_id):
+        if Memo.objects.filter(id=memo_id).exists():
+            memo = Memo.objects.get(id=memo_id)
+            self.check_object_permissions(self.request, memo)
+            return memo
+        return None
+
+    def get(self, request, memo_id):
+        memo = self.get_object(memo_id)
+        if memo is None:
+            return Response({'message': 'invalid memo id'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = MemoSerializer(memo)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
