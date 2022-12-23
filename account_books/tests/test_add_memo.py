2737465fc0409a2c989a23b028be078685fc2620
rla_thdus <0228ksy@naver.com>
2022-12-23 21:34:35 +0900

    Test: 메모 생성 요청에 충분한 데이터가 없으면 실패하는 테스트 추가

--- account_books/tests/test_add_memo.py
+++ account_books/tests/test_add_memo.py
@@ -25,3 +25,9 @@ class MemoAddAPITest(APITestCase):
     def test_add_memo_should_fail_with_not_authenticated_user(self):
         response = self.client.post('/account_books/memos', self.data)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_add_memo_should_fail_with_invalid_data(self):
+        self.data['spend_price'] = ''
+        headers = {'HTTP_AUTHORIZATION': "Bearer " + json.loads(self.login.content)['access_token']}
+        response = self.client.post('/account_books/memos', self.data, **headers)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
