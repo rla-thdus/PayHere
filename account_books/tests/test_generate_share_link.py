be0bc3efc1827b86f7402bc00ead27cae4d1f669
rla_thdus <0228ksy@naver.com>
2022-12-24 21:10:46 +0900

    Test: 메모 공유 링크 생성하는 테스트 추가

---
+++ account_books/tests/test_generate_share_link.py
@@ -0,0 +1,38 @@
+import json
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class GenerateMemoShareLinkAPITest(APITestCase):
+    def setUp(self):
+        user_data = {
+            "email": "test@test.com",
+            "password": "test1234",
+        }
+        new_user_data = {
+            "email": "test1@test.com",
+            "password": "test1234",
+        }
+        self.client.post('/accounts/registration', user_data)
+        self.client.post('/accounts/registration', new_user_data)
+        self.login = self.client.post('/accounts/login', user_data)
+        self.new_login = self.client.post('/accounts/login', new_user_data)
+        self.data = {
+            "spend_price": 10000,
+            "content": 'buy snacks'
+        }
+        self.data2 = {
+            "spend_price": 1000,
+            "content": 'buy snacks'
+        }
+        self.headers = {'HTTP_AUTHORIZATION': f"Bearer {json.loads(self.login.content)['access']}"}
+        memo = self.client.post('/account_books/memos', self.data, **self.headers)
+        self.client.post('/account_books/memos', self.data2, **self.headers)
+        self.memo_id = memo.data['id']
+
+    def test_generate_memo_share_link_should_success_with_authenticated_user(self):
+        headers = {'HTTP_AUTHORIZATION': f"Bearer {json.loads(self.login.content)['access']}"}
+        response = self.client.post(f'/account_books/share/memos/{self.memo_id}', **headers)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue('link' in response.data)
