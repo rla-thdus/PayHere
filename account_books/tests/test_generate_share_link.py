a855b1b8f3044a1facdb13f2cc457ffbd2a262f4
rla_thdus <0228ksy@naver.com>
2022-12-24 21:15:39 +0900

    Test: 메모 공유 링크 실패하는 테스트 추가

--- account_books/tests/test_generate_share_link.py
+++ account_books/tests/test_generate_share_link.py
@@ -36,3 +36,17 @@ class GenerateMemoShareLinkAPITest(APITestCase):
         response = self.client.post(f'/account_books/share/memos/{self.memo_id}', **headers)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertTrue('link' in response.data)
+
+    def test_generate_memo_share_link_should_fail_with_unauthorized_user(self):
+        response = self.client.post(f'/account_books/share/memos/{self.memo_id}')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_generate_memo_share_link_should_fail_with_not_own_memo(self):
+        headers = {'HTTP_AUTHORIZATION': f"Bearer {json.loads(self.new_login.content)['access']}"}
+        response = self.client.post(f'/account_books/share/memos/{self.memo_id}', **headers)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_generate_memo_share_link_should_fail_with_does_not_exist_memo(self):
+        headers = {'HTTP_AUTHORIZATION': f"Bearer {json.loads(self.login.content)['access']}"}
+        response = self.client.post(f'/account_books/share/memos/{self.memo_id + 100}', **headers)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
